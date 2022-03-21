2ae9c0012399ca1edb23a6b1727e058845a93f18
Abhinav Sharma <abhi18av@gmail.com>
2022-03-21 20:08:56 +0200

    update conda command and put the test workflow in the end

--- conda_envs/setup_conda_envs.sh
+++ conda_envs/setup_conda_envs.sh
@@ -2,10 +2,10 @@
 
 set -xue
 
-# NOTE: These environments must be in /path/to/xbs-nf/conda_envs folder
+# NOTE: Please replace `conda` with `mamba` if it is installed for faster installs.
 
-# NOTE: If there are problems in `mamba`, replace it with `conda`
+# NOTE: The conda environments are expected by the `conda_local` profile to be created within `/path/to/xbs-nf/conda_envs` directory
 
-mamba create -p ./xbs-nf-env-1 bioconda::gatk4=4.2.4.1 conda-forge::R=4.1  conda-forge::r-ggplot2=3.3.5 bioconda::datamash=1.1.0 bioconda::delly=0.8.7 bioconda::lofreq=2.1.5 bioconda::delly=0.8.7 bioconda::lofreq=2.1.5 bioconda::tb-profiler=4.1.1 bioconda::multiqc=1.11 bioconda::fastqc=0.11.8
+conda create -p ./xbs-nf-env-1 bioconda::gatk4=4.2.4.1 conda-forge::R=4.1  conda-forge::r-ggplot2=3.3.5 bioconda::datamash=1.1.0 bioconda::delly=0.8.7 bioconda::lofreq=2.1.5 bioconda::delly=0.8.7 bioconda::lofreq=2.1.5 bioconda::tb-profiler=4.1.1 bioconda::multiqc=1.11 bioconda::fastqc=0.11.8
 
-mamba create -p ./xbs-nf-env-2 jemunro::quanttb=1.01 bioconda::bwa=0.7.17 bioconda::samtools=1.9 bioconda::iqtree=2.1.2 bioconda::snp-dists=0.8.2 bioconda::snp-sites=2.4.0 bioconda::bcftools=1.9 bioconda::snpeff=4.3.1t bioconda::clusterpicker=1.2.3
+conda create -p ./xbs-nf-env-2 jemunro::quanttb=1.01 bioconda::bwa=0.7.17 bioconda::samtools=1.9 bioconda::iqtree=2.1.2 bioconda::snp-dists=0.8.2 bioconda::snp-sites=2.4.0 bioconda::bcftools=1.9 bioconda::snpeff=4.3.1t bioconda::clusterpicker=1.2.3
