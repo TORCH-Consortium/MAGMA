b1b13c85b26d85593ed7002868497c5352419020
Abhinav Sharma <abhi18av@gmail.com>
2022-03-21 23:22:42 +0200

    tweak the conda env files for general conda-forge channel

--- containers/build.sh
+++ containers/build.sh
@@ -11,7 +11,7 @@ cp ../conda_envs/xbs-nf-env-2.yml ./xbs-nf-container-2
 for container_dir in $(find * -type d); do
   echo "Building $container_dir ..."
   cd $container_dir
-  CONTAINER_TAG=0.4.0
+  CONTAINER_TAG=0.5.0
   CONTAINER_NAME=$DOCKER_NAMESPACE/$container_dir:$CONTAINER_TAG
   echo "Container Name : $CONTAINER_NAME "
   docker build -t $CONTAINER_NAME .
