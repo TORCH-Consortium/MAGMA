03fa87eaab55ea9e7782046f8070ff1ad42bb895
Abhinav Sharma <abhi18av@gmail.com>
2022-03-21 21:27:20 +0200

    create a minimal conda env script

--- conda_envs/setup_conda_envs.sh
+++ conda_envs/setup_conda_envs.sh
@@ -4,8 +4,8 @@ set -xue
 
 # NOTE: Please replace `conda` with `mamba` if it is installed for faster installs.
 
-# NOTE: The conda environments are expected by the `conda_local` profile to be created within `/path/to/xbs-nf/conda_envs` directory
+# NOTE: The conda environments are expected by the `conda_local` profile to be created within `xbs-nf/conda_envs` directory
 
-conda create -p ./xbs-nf-env-1 bioconda::gatk4=4.2.4.1 conda-forge::R=4.1  conda-forge::r-ggplot2=3.3.5 bioconda::datamash=1.1.0 bioconda::delly=0.8.7 bioconda::lofreq=2.1.5 bioconda::delly=0.8.7 bioconda::lofreq=2.1.5 bioconda::tb-profiler=4.1.1 bioconda::multiqc=1.11 bioconda::fastqc=0.11.8
+conda env create -p xbs-nf-env-1 --file xbs-nf-env-1.yml
 
-conda create -p ./xbs-nf-env-2 jemunro::quanttb=1.01 bioconda::bwa=0.7.17 bioconda::samtools=1.9 bioconda::iqtree=2.1.2 bioconda::snp-dists=0.8.2 bioconda::snp-sites=2.4.0 bioconda::bcftools=1.9 bioconda::snpeff=4.3.1t bioconda::clusterpicker=1.2.3
+conda env create -p xbs-nf-env-2 --file xbs-nf-env-2.yml
