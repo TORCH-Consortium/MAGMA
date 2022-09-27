01f8caaba10a711c3a18c5ad6c00583594882fdb
LennertVerboven <lennert.verboven@uantwerpen.be>
2022-09-27 12:32:48 +0200

    Create reformat_lofreq.py
    Added the reformatting for lofreq vcf files

---
+++ bin/reformat_lofreq.py
@@ -0,0 +1,53 @@
+import ast
+import argparse
+
+import pandas as pd
+
+header_formats = """##FORMAT=<ID=AD,Number=R,Type=Integer,Description="Allelic depths for the ref and alt alleles in the order listed">
+##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Approximate read depth (reads with MQ=255 or with bad mates are filtered)">
+##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
+##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+##FORMAT=<ID=MIN_DP,Number=1,Type=Integer,Description="Minimum DP observed within the GVCF block">
+##FORMAT=<ID=PL,Number=G,Type=Integer,Description="Normalized, Phred-scaled likelihoods for genotypes as defined in the VCF specification">
+"""
+
+def read_vcf(filename):
+    with open(filename, 'r') as vcf:
+        header = ''
+        for line in vcf:
+            if line[:2] != '##':
+                break
+            else:
+                header += line
+        df = pd.read_csv(vcf, header=None, sep='\t')
+    df.columns = line[:-1].split('\t')
+    return df, header
+
+def write_vcf(filename, df, header):
+    with open(filename, 'w') as vcf:
+        vcf.write(header)
+        df.to_csv(vcf, sep='\t', index=False)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Analyse resistance output from XBS Pipeline')
+    parser.add_argument('lofreq_vcf_file', metavar='lofreq_vcf_file', type=str, help='The input lofreq vcf file')
+    parser.add_argument('lofreq_sample_name', metavar='lofreq_sample_name', type=str, help='The sample name')
+    parser.add_argument('outfile', metavar='literature_db', type=str, help='TThe name of the output VCF file')
+
+    args = vars(parser.parse_args())
+
+    vcf, header = read_vcf(args['lofreq_vcf_file'])
+    vcf['FORMAT'] = 'GT:AD:DP:GQ:PL'
+
+    for idx, row in vcf.iterrows():
+        info = [ast.literal_eval(i.split('=')[1]) for i in row['INFO'].split(';')[:4]]
+        ref_dp = sum(info[3][:2])
+        alt_dp = sum(info[3][2:])
+        GT = 1
+        AD = '{},{}'.format(ref_dp, alt_dp)
+        DP = sum(info[3])
+        GQ = 99
+        PL = '1800,0'
+        vcf.loc[idx, args['lofreq_sample_name']] = '{}:{}:{}:{}:{}'.format(GT,AD,DP,GQ,PL)
+
+    write_vcf(args['outfile'], vcf, header)
