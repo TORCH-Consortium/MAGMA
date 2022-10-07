209c2cdf24faaa712f87f42b833b7dce38a143a3
Abhinav Sharma <abhi18av@gmail.com>
2022-10-07 07:16:33 +0200

    update conda env file

--- conda_envs/setup_conda_envs.sh
+++ conda_envs/setup_conda_envs.sh
@@ -3,9 +3,18 @@
 set -xue
 
 # NOTE: Please replace `conda` with `mamba` if it is installed for faster installs.
+condaBinary="conda" # OR mamba
 
-# NOTE: The conda environments are expected by the `conda_local` profile to be created within `xbs-nf/conda_envs` directory
+# NOTE: By default, the conda environments are expected by the `conda_local` profile to be created within `xbs-nf/conda_envs` directory
 
-conda env create -p xbs-nf-env-1 --file xbs-nf-env-1.yml
+$condaBinary env create -p xbs-nf-env-1 --file xbs-nf-env-1.yml
 
-conda env create -p xbs-nf-env-2 --file xbs-nf-env-2.yml
+$condaBinary env create -p xbs-nf-env-2 --file xbs-nf-env-2.yml
+
+#NOTE: Setup the WHO database
+$condaBinary activate "./xbs-nf-env-1"
+cp -r ../resources/resistance_db_who ./
+cd resistance_db_who
+tb-profiler load_library resistance_db_who
+rm -rf resistance_db_who ./
+cd ..
