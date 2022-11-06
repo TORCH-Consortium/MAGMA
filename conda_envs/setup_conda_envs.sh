9297a06d24ef87e758c78a2baa59724b36af9f6b
Abhinav Sharma <abhi18av@gmail.com>
2022-11-06 11:14:58 +0200

    refactor the usage of xbs-nf name

--- conda_envs/setup_conda_envs.sh
+++ conda_envs/setup_conda_envs.sh
@@ -7,13 +7,13 @@ resolverCondaBinary="conda" # pick either conda OR mamba
 
 # NOTE: By default, the conda environments are expected by the `conda_local` profile to be created within `magma/conda_envs` directory
 
-$resolverCondaBinary env create -p magma-nf-env-1 --file xbs-nf-env-1.yml 
+$resolverCondaBinary env create -p magma-nf-env-1 --file magma-env-1.yml 
 
-$resolverCondaBinary env create -p magma-nf-env-2 --file xbs-nf-env-2.yml
+$resolverCondaBinary env create -p magma-nf-env-2 --file magma-env-2.yml
 
-echo "INFO: Activate conda env with tb-profiler and setup the WHO database within the xbs-nf-env-1"
+echo "INFO: Activate conda env with tb-profiler and setup the WHO database within the magma-env-1"
 eval "$(conda shell.bash hook)"
-conda activate "./xbs-nf-env-1"
+conda activate "./magma-env-1"
 
 echo "INFO: Make a local copy and cd inside it"
 cp -r ../resources/resistance_db_who ./
@@ -26,5 +26,5 @@ echo "INFO: Remove the local copy of the database"
 cd ..
 rm -rf resistance_db_who
 
-echo "INFO: Deactivate the xbs-nf-env-1 env"
+echo "INFO: Deactivate the magma-env-1 env"
 conda deactivate
