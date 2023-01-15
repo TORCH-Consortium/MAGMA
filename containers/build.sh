9d77c85996d6d11f9fa5e5e781cb0c68932588b2
Abhinav Sharma <abhi18av@gmail.com>
2023-01-15 16:26:55 +0200

    update the build number

--- containers/build.sh
+++ containers/build.sh
@@ -13,7 +13,7 @@ cp ../conda_envs/magma-env-2.yml ./magma-container-2
 for container_dir in $(find * -maxdepth 0 -type d); do
   echo "Building $container_dir ..."
   cd $container_dir
-  CONTAINER_TAG=0.9.10
+  CONTAINER_TAG=1.0.0
   CONTAINER_NAME=$DOCKER_NAMESPACE/$container_dir:$CONTAINER_TAG
   echo "Container Name : $CONTAINER_NAME "
   docker build -t $CONTAINER_NAME .
