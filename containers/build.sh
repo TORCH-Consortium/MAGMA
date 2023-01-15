52c8d38842584e2104872cf6797577f364192df1
Abhinav Sharma <abhi18av@gmail.com>
2023-01-15 15:24:32 +0200

    update docker container folders

--- containers/build.sh
+++ containers/build.sh
@@ -3,12 +3,12 @@ set -uex
 
 # NOTE: Make sure you've set the environment correctly and are logged in to the registry.
 
-DOCKER_NAMESPACE="rg.nl-ams.scw.cloud/xbs-nf-containers"
+DOCKER_NAMESPACE="rg.nl-ams.scw.cloud/magma-containers"
 
-cp ../conda_envs/xbs-nf-env-1.yml ./xbs-nf-container-1
-cp -r ../resources/resistance_db_who ./xbs-nf-container-1
+cp ../conda_envs/magma-env-1.yml ./magma-container-1
+cp -r ../resources/resistance_db_who ./magma-container-1
 
-cp ../conda_envs/xbs-nf-env-2.yml ./xbs-nf-container-2
+cp ../conda_envs/magma-env-2.yml ./magma-container-2
 
 for container_dir in $(find * -maxdepth 0 -type d); do
   echo "Building $container_dir ..."
