e590d85674a0431669a0d013f9fc92e7df481271
Abhinav Sharma <abhi18av@gmail.com>
2023-08-13 14:54:19 +0200

    replace sed -> python scripts [ci skip]

--- bin/rename_vcf_chrom.py
+++ bin/rename_vcf_chrom.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python2
 
 '''Original author Jody Phelan at https://github.com/jodyphelan/pathogen-profiler/blob/master/scripts/rename_vcf_chrom.py'''
 import sys
