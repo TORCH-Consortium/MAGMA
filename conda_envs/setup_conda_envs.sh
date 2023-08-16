dbbad0df36eca09499b43dc031343badbb9d528d
LennertVerboven <lennert.verboven@uantwerpen.be>
2023-08-16 14:17:43 +0200

    Update setup_conda_envs.sh

--- conda_envs/setup_conda_envs.sh
+++ conda_envs/setup_conda_envs.sh
@@ -3,7 +3,7 @@
 set -e
 
 # NOTE: Please replace `conda` with `mamba` if it is installed for faster installs.
-resolverCondaBinary="mamba" # pick either conda OR mamba
+resolverCondaBinary="conda" # pick either conda OR mamba
 
 # NOTE: By default, the conda environments are expected by the `conda_local` profile to be created within `magma/conda_envs` directory
 
