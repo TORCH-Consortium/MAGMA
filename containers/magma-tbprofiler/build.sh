23810de0799be198153c9a27393221d98b89c3d2
Abhinav Sharma <abhi18av@gmail.com>
2024-03-22 16:58:30 -0300

    remove ntm related code to clean branch [ci skip]

---
+++ containers/magma-tbprofiler/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -uex
+
+# NOTE: Make sure you've set the environment correctly and are logged in to the registry.
+#
+
+CONTAINER_TAG=6.0.0
+DOCKER_NAMESPACE="ghcr.io/torch-consortium/magma"
+
+CONTAINER_NAME="$DOCKER_NAMESPACE/magma-tbprofiler:$CONTAINER_TAG"
+
+echo "Building container : $CONTAINER_NAME "
+
+cp ../../conda_envs/magma-env-2.yml ./
+
+
+docker build -t $CONTAINER_NAME .
+CONTAINER_ID=$(docker run -d $CONTAINER_NAME)
+docker commit $CONTAINER_ID $CONTAINER_NAME
+docker push $DOCKER_NAMESPACE/$container_dir:$CONTAINER_TAG
+docker stop $CONTAINER_ID
