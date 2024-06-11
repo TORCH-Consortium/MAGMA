73ddd659ffe77473302fd982f5f60dda59ca9d6c
Miguel de Diego Fuertes <118442908+mdediegofuertes@users.noreply.github.com>
2024-06-11 12:23:54 +0200

    Update summarize_resistance.py
    Ensure that all relevant DR_variants are reported for every drug, regardless of whether a WHO classification exists for them or not.

--- bin/summarize_resistance.py
+++ bin/summarize_resistance.py
@@ -48,6 +48,12 @@ def create_resistance_df(sample_res, method):
         gene = var['gene_name']
         var_repr = '{}_{}'.format(gene, var['change'])
 
+        # Add variants with an unlisted classification and overwrite later if necessary
+        for drug in var['gene_associated_drugs']:
+            drug_name = drug.lower().replace(' ', '_')
+            pt_df.loc[(drug_name, var_repr), ('Resistance interpretation', 'Source', 'Source notation', 'Type', 'Frequency', 'Literature')] = [unknown_positions, 'non-Catalogue', '', var['type'], '{:.0%}'.format(var['freq']), 'Manually curated']
+
+        # Overwrite the variant classification for drugs which have a WHO classification last as to overrule unlisted classifications
         for drug in var['drugs']:
             drug_name = drug['drug'].lower().replace(' ', '_')
             pt_df.loc[(drug_name, var_repr), ('Resistance interpretation', 'Source', 'Source notation', 'Type', 'Frequency', 'Literature')] = [map_confidence_who[drug['confidence']], 'Catalogue', drug['original_mutation'], var['type'], '{:.0%}'.format(var['freq']), drug['source']]
