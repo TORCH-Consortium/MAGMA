c6847024b800a1076a61d648b2286778537c4804
Abhinav Sharma <abhi18av@gmail.com>
2024-06-17 15:34:20 +0200

    add CLI interface to the script

--- bin/reduce_strand_bias.py
+++ bin/reduce_strand_bias.py
@@ -1,55 +1,55 @@
-import vcf
+#!/usr/bin/env python3
 
+import argparse
+import vcf
 from scipy.stats import binom_test
 
-# Path to your VCF file
 
-vcf_file = "your_file.vcf"  # Replace "your_file.vcf" with the actual file name
+def filter_vcf_file(vcf_input, vcf_output, p_value=0.5):
 
-# Open the VCF file for reading
+    # Open the VCF file for reading
 
-vcf_reader = vcf.Reader(open(vcf_file, 'r'))
+    vcf_reader = vcf.Reader(open(vcf_input, 'r'))
 
-# Create a list to store the filtered records
+    # Create a list to store the filtered records
 
-filtered_records = []
+    filtered_records = []
 
-# Iterate over each record (variant) in the VCF file
+    # Iterate over each record (variant) in the VCF file
 
-for record in vcf_reader:
+    for record in vcf_reader:
 
-    # Extract the DP4 field from the INFO dictionary
+        # Extract the DP4 field from the INFO dictionary
 
-    dp4_values = record.INFO.get('DP4')
+        dp4_values = record.INFO.get('DP4')
 
-    # Perform binomial test on the last two values
+        # Perform binomial test on the last two values
 
-    if dp4_values:
+        if dp4_values:
 
-        p_value = binom_test([dp4_values[-2], dp4_values[-1]], n=sum(dp4_values[-2:]), p=0.5)
+            p_value = binom_test([dp4_values[-2], dp4_values[-1]], n=sum(dp4_values[-2:]), p=p_value)
 
-        # Check if p-value is above or equal to 0.05
+            # Check if p-value is above or equal to 0.05
+            if p_value >= p_value:
+                filtered_records.append(record)
 
-        if p_value >= 0.05:
+    # Write the filtered records to a new VCF file
+    vcf_writer = vcf.Writer(open(vcf_output, 'w'), vcf_reader)
 
-            filtered_records.append(record)
+    for record in filtered_records:
+        vcf_writer.write_record(record)
 
-# Write the filtered records to a new VCF file
-
-output_vcf = "filtered_output.vcf"
-
-vcf_writer = vcf.Writer(open(output_vcf, 'w'), vcf_reader)
-
-
-for record in filtered_records:
-
-    vcf_writer.write_record(record)
-
-# Close the VCF writer
-
-vcf_writer.close()
+    # Close the VCF writer
+    vcf_writer.close()
 
 
-print("Filtered VCF file has been created successfully.")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Reduce strand bias in VCF file')
+    parser.add_argument('input', metavar='input_vcf', type=str, help='The initial VCF file')
+    parser.add_argument('output', metavar='output_vcf', type=str, help='The output VCF filed')
+    args = vars(parser.parse_args())
 
+    input_vcf_file = args['input_vcf']
+    output_vcf_file = args['output_vcf']
 
+    filter_vcf_file(input_vcf_file, output_vcf_file)
