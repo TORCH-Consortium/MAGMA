2c69432dba97ae3147d05d117f70db51afeddf74
Abhinav Sharma <abhi18av@gmail.com>
2024-06-22 17:08:48 +0200

    dev iteration

--- bin/samplesheet_validation.py
+++ bin/samplesheet_validation.py
@@ -7,11 +7,15 @@ import pandas as pd
 from sys import exit
 
 parser = argparse.ArgumentParser(description='Run the MAGMA pipeline samplesheet validation')
-parser.add_argument('input_file', metavar='input_file', type=str, help='The input sample file')
+parser.add_argument('input', metavar='input_file', type=str, help='The input sample file')
+parser.add_argument('output', metavar='output_file', type=str, help='The validate output sample file')
 args = vars(parser.parse_args())
 
+input_file = args['input_file']
+output_file = args['input_file']
+
 name_re = re.compile('^[a-zA-Z0-9\-\_]*$')
-ss = pd.read_csv(args['input_file'])
+ss = pd.read_csv(input_file)
 
 # Create another column by adding Sample and Attempt columns
 ss['MagmaSampleName'] = ss['Study'].astype(str) + \
@@ -32,12 +36,12 @@ for idx, row in ss.iterrows():
         print('Row {}: {} - Illegal character in SAMPLE id'.format(idx, row['Sample']))
         fail = True
     if row['R1'] == row['R2']:
-        print('Row {}: {}, {} - REPEATED fastq file specified'.format(idx, row['R1'], row['R2']))
+        print('Row {}: {}, {} - DUPLICATED fastq file specified'.format(idx, row['R1'], row['R2']))
         fail = True
 
 
 if not fail:
-    ss.to_csv('samplesheet.format_valid.csv', index=False)
+    ss.to_csv(output_file, index=False)
     print('Samplesheet format validation checks passed')
     exit(0)
 else:
