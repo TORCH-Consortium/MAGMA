c23a772c35e497bd0653333f16ac3b4783ec2a08
Abhinav Sharma <abhi18av@outlook.com>
2024-08-23 16:15:31 +0200

    disable stdout [ci skip]

--- bin/rename_vcf_chrom.py
+++ bin/rename_vcf_chrom.py
@@ -32,18 +32,22 @@ def cmd_out(cmd,verbose=1):
 def main(args):
     generator = cmd_out("bcftools view " + args.vcf) if args.vcf else sys.stdin
     convert = dict(zip(args.source,args.target))
-    for l in generator:
-        if l[0]=="#":
-            sys.stdout.write(l.strip()+"\n")
-        else:
-            row = l.strip().split()
-            row[0] = convert[row[0]]
-            sys.stdout.write("\t".join(row)+"\n")
+
+    # Open the output file for writing
+    with open(args.outfile, 'w') as outfile:
+        for l in generator:
+            if l[0]=="#":
+                outfile.write(l.strip()+"\n")
+            else:
+                row = l.strip().split()
+                row[0] = convert[row[0]]
+                outfile.write("\t".join(row)+"\n")
 
 parser = argparse.ArgumentParser(description='tbprofiler script',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--vcf',type=str,help='')
 parser.add_argument('--source',nargs="+",type=str,help='')
 parser.add_argument('--target',nargs="+",type=str,help='')
+parser.add_argument('--outfile', type=str, required=True, help='Output VCF file')
 parser.set_defaults(func=main)
 args = parser.parse_args()
 args.func(args)
--- modules/snpeff/snpeff.nf
+++ modules/snpeff/snpeff.nf
@@ -13,11 +13,11 @@ process SNPEFF {
     shell:
 
         '''
-        rename_vcf_chrom.py --vcf !{rawJointVariantsFile}  --source !{params.ref_fasta_basename} --target 'Chromosome' > temp.chromosomes.vcf
+        rename_vcf_chrom.py --vcf !{rawJointVariantsFile}  --source !{params.ref_fasta_basename} --target 'Chromosome' --outfile temp.chromosomes.vcf
 
         !{params.snpeff_path} -nostats !{params.arguments} temp.chromosomes.vcf > temp.annotated.vcf
 
-        rename_vcf_chrom.py --target !{params.ref_fasta_basename} --source 'Chromosome' --vcf temp.annotated.vcf > !{joint_name}.temp.vcf
+        rename_vcf_chrom.py --target !{params.ref_fasta_basename} --source 'Chromosome' --vcf temp.annotated.vcf --outfile !{joint_name}.temp.vcf
 
          cp !{joint_name}.temp.vcf  !{joint_name}.raw_variants.annotated.vcf
         '''
